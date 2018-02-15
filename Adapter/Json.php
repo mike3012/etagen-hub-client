5227a24a71941d58681c4a417da462c7a07c5420
Mike Stübbe <stuebbe@big5.de>
2018-02-15 12:49:22 +0100

    to bundle ...

--- Adapter/Json.php
+++ Adapter/Json.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EtagenHubClient\Adapter;
+namespace EtagenHubClientBundle\Adapter;
 
 class Json
 {
---
+++ EtagenHubClientBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EtagenHubClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EtagenHubClientBundle extends Bundle
+{
+}
