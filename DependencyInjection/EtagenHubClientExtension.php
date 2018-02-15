9f31ec54d55fd0da5a2dbb3f646d4d682edec383
Mike Stübbe <stuebbe@big5.de>
2018-02-15 12:57:34 +0100

    make service public

---
+++ DependencyInjection/EtagenHubClientExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EtagenHubClientBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class EtagenHubClientExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
